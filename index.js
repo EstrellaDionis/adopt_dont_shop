e8e5ef5cabe405fe38f080757b009f99aa3376ea
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-03 17:46:59 -0400

    installed dotenv and implementing it in index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-console.log(process.argv); //npm run start  --web-48 web-49 web-50
+// console.log(process.argv); //npm run start  --web-48 web-49 web-50
 
-const cohort1 = process.argv[2];
-const display = process.env.DISPLAY; //grabbed this by typing env in the console and choosing a property name
+// const cohort1 = process.argv[2];
+// const display = process.env.DISPLAY; //grabbed this by typing env in the console and choosing a property name
 
-if (cohort1 === "web-49") {
-  console.log(`${cohort1} is the best forever`);
-} else {
-  console.log(`booh cohort ${cohort1}`);
-}
+// if (cohort1 === "web-49") {
+//   console.log(`${cohort1} is the best forever`);
+// } else {
+//   console.log(`booh cohort ${cohort1}`);
+// }
 
-console.log(`the display is ${display}`);
+// console.log(`the display is ${display}`);
 
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.get("/hello", (req, res) => {
   res.json({ message: "hey there" });
 });
 
-const port = 9000;
+const port = process.env.PORT || 9000; //heroku wants to set its own port
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
