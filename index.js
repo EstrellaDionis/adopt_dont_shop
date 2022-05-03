64307a8e772f5e56036821c83f55590c95ab32b5
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-03 16:46:12 -0400

    fixed eslint configuration

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("web");
+console.log(process.argv); //npm run start  --web-48 web-49 web-50
+
+const cohort1 = process.argv[2];
+const display = process.env.DISPLAY; //grabbed this by typing env in the console and choosing a property name
+
+if (cohort1 === "web-49") {
+  console.log(`${cohort1} is the best forever`);
+} else {
+  console.log(`booh cohort ${cohort1}`);
+}
+
+console.log(`the display is ${display}`);
