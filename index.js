fd1212fd05d21b3cc14b6b8ce4e1dcfd44ca1a86
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-05 20:33:45 -0400

    GOT DATABASE INFORMATION SHOWING IN PETS.EJS!!!

--- index.js
+++ index.js
@@ -3,23 +3,46 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const sqlite3 = require("sqlite3");
+const ejsMate = require("ejs-mate");
+const sqlite3 = require("sqlite3").verbose();
 
-// .static takes absolute path to 'build' folder
-app.use(express.static(path.join(__dirname, "client/src")));
-app.use(cors());
-app.use(express.json());
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//connection to SQlite DB
+const db_name = path.join(__dirname, "data", "pets.db3");
+const db = new sqlite3.Database(db_name, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Succesful connection to the database 'pets.db3'");
+});
 
 app.get("/", (req, res) => {
-  res.send("Hey this is the data for animals");
+  res.render("home");
 });
 
-app.get("/hello", (req, res) => {
-  res.json({ message: "hey there" });
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get("/animals", (req, res) => {
-  res.send("Animals page");
+app.get("/data", (req, res) => {
+  const test = {
+    titre: "Test",
+    items: ["one", "two", "three"],
+  };
+  res.render("data", { model: test });
+});
+
+app.get("/pets", (req, res) => {
+  const sql = "SELECT * FROM pets ORDER BY name";
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("pets", { model: rows });
+  });
 });
 
 const port = process.env.PORT || 9000; //heroku wants to set its own port
